dad1b0174749109ca93d5cba07eb5c2e2aa4de05
Pingvin0 <58985574+Pingvin0@users.noreply.github.com>
2020-01-17 14:24:10 +0100

    Add files via upload

--- 2fa.py
+++ 2fa.py
@@ -2,12 +2,26 @@ from machine import Pin,deepsleep,RTC
 import machine
 import esp32
 from utime import sleep
-import ntptime
 import socket
 import utime
+import sys
+import m5stack
+from m5stack import lcd
+
+sys.path.insert(1, '')
+import ntptime
 wlssid = "TP-Link_MAIN_Guest"
 wlpw = "1964370737"
+rtc = machine.RTC()
 
+
+def turnOnScreen():
+    m5stack.lcd.clear()
+    m5stack.axp.setLcdBrightness(35)
+
+def turnOffScreen():
+    m5stack.lcd.clear()
+    m5stack.axp.setLcdBrightness(0)
 
 def connectToWifi():
     import network
@@ -16,15 +30,10 @@ def connectToWifi():
     ap_if = network.WLAN(network.AP_IF)
     sta_if.active(True)
     sta_if.connect(wlssid, wlpw)
-    led = Pin(2, Pin.OUT)
     while sta_if.isconnected() == False:
         print("Unconnected to wifi")
-        led.value(1)
-        sleep(0.2)
-        led.value(0)
         sleep(0.2)
     print("Successful connection to " + wlssid)
-    led.value(0)
     sendConnectedToMQTT()
 
 def http_get(url):
@@ -36,25 +45,27 @@ def http_get(url):
     while True:
         data = s.recv(100)
         if data:
-            print(str(data, 'utf8'), end='')
+            print("Got data from http")
         else:
             break
+    print("Sent httpget")
     s.close()
 
 def syncTime():
     ntptime.settime()
 
 def goSleepBoi():
-    wakePin =  Pin(0, Pin.IN, Pin.PULL_DOWN)
+    wakePin =  Pin(37, Pin.IN, Pin.PULL_DOWN)
     esp32.wake_on_ext0(wakePin, esp32.WAKEUP_ALL_LOW)
-    print("Going to sleep in 5s")
-    sleep(5)
+    print("Going to sleep in 3s")
+    sleep(3)
     print("Going to sleep")
+    turnOffScreen()
     deepsleep()
 
 def sendTimeToMQTT():
-    year,month,day,idk,hour,minute,second,microsecond = RTC().datetime()
-    http_get("https://api.thingspeak.com/update?api_key=45TGY41GRUYV83SK&field1=" + str(hour) + ":" + str(minute))
+    year,month,day,idk,hour,minute,second,microsecond = utime.localtime()
+    http_get("https://api.thingspeak.com/update?api_key=45TGY41GRUYV83SK&field1=" + str(hour) + "H" + str(minute) + "M")
     
 def sendSleepToMQTT():
     http_get("https://api.thingspeak.com/update?api_key=45TGY41GRUYV83SK&field2=1")
@@ -63,20 +74,21 @@ def sendConnectedToMQTT():
     http_get("https://api.thingspeak.com/update?api_key=45TGY41GRUYV83SK&field3=1")
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
-    led = Pin(2, Pin.OUT)
-    year,month,day,idk,hour,minute,second,microsecond = RTC().datetime()
+    turnOnScreen()
+    lcd.text(0, 0, "DISCORD")
+    lcd.text(lcd.CENTER, lcd.CENTER, "AUTHENTICATOR CODE HERE")
+    year,month,day,idk,hour,minute,second,microsecond = utime.localtime()
     if minute < 10:
         minute = "0" + str(minute)
     print('woke from a deep sleep time is: ' + str(hour) + ":" + str(minute) + " And the date is (YYYY/MM/DD) " + str(year) + "/" + str(month) + "/" + str(day))
-    led.value(1)
+    goSleepBoi()
 else:
     print('power on or hard reset')
     
     connectToWifi()
     syncTime()
     sendTimeToMQTT()
-    print("Current time detected as: " +  str(RTC().datetime()))
+    print("Current time detected as: " + str(utime.localtime()))
     
     sendSleepToMQTT()
     goSleepBoi()
-
